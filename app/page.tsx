fb0d27c920f0b7a659c7d02021a470c985dc8d89
radhikashrivastava <radhika.shrivastava@workato.com>
2026-01-05 17:39:40 +0530

    urls changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  const handleClick = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then(res => res.json())
-      .then(data => console.log(data));
-  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -64,9 +60,7 @@ export default function Home() {
             Documentation
           </a>
         </div>
-        <button onClick={handleClick}>
-      Send Request
-    </button>
+       
       </main>
     </div>
   );
