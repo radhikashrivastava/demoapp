ff993f6de0e851750710c968d4e35eb2bf8ab0c8
radhikashrivastava <radhika.shrivastava@workato.com>
2026-01-05 17:36:34 +0530

    urls changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
+  const handleClick = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -37,7 +42,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://www.youtube.com/"
+            href="https://jsonplaceholder.typicode.com/todos/1"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -48,7 +53,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Click
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
@@ -59,6 +64,9 @@ export default function Home() {
             Documentation
           </a>
         </div>
+        <button onClick={handleClick}>
+      Send Request
+    </button>
       </main>
     </div>
   );
